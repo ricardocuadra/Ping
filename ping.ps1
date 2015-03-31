feefd29b3b2f76df54e4f03e142294490e2d4e88
Ricardo Cuadra <ricardo.cuadra@it-gymnasiet.se>
2015-03-31 11:31:49 +0200

    Fixade indentering

--- ping.ps1
+++ ping.ps1
@@ -2,20 +2,17 @@
 $a = Get-NetAdapter  
 $i = 0 
 $datorer =1..255
-foreach($dator in $datorer) { $ip = "10.130.196.$dator"  
-$Res = $ping.send($ip) 
+foreach($dator in $datorer) { 
+    $ip = "10.130.196.$dator"  
+    $Res = $ping.send($ip) 
  
-if ($Res.Status -eq "Success") 
-{ 
-$dnamn =[Net.DNS]::GetHostEntry($ip)
-$result = $ip + " = $dnamn" 
-Write-Host $result 
+    if ($Res.Status -eq "Success") { 
+        $dnamn =[Net.DNS]::GetHostEntry($ip)
+        $result = $ip + " = $dnamn" 
+        Write-Host $result 
  
-$i++ 
- 
-} 
- 
- 
+        $i++ 
+    } 
 }  
 $Hosts = [string]$ip + " Hosts is pingable" 
 Write-Host $Hosts 
